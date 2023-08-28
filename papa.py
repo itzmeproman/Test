73332035a7a8e39a7c2bfd9598676278a25935ed
itzmeproman <atulpandey3030@gmail.com>
2023-08-28 20:06:38 +0530

    Update papa.py

--- papa.py
+++ papa.py
@@ -6,12 +6,15 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # Define the bot token and API keys
+# You should not expose your API keys and bot token in your code
+# Use environment variables or a config file instead
 API_ID = os.environ.get("20210345")
 API_HASH = os.environ.get("11bcb58ae8cfb85168fc1f2f8f4c04c2")
 BOT_TOKEN = os.environ.get("6154222206:AAFxkaTRgMI52biIT3m4qAUDwsWIySnoY2c")
 
 # Create a bot instance
-bot = Client("video_encoder_bot", api_id=20210345, api_hash=0x11bcb58ae8cfb85168fc1f2f8f4c04c2, bot_token="6154222206:AAFxkaTRgMI52biIT3m4qAUDwsWIySnoY2c")
+# You should use the API_ID and API_HASH variables here instead of hard-coding them
+bot = Client("video_encoder_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Define the download directory and the ffmpeg command
 DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "downloads/")
@@ -51,7 +54,8 @@ def encode_video(file_path):
     output_file = os.path.join(DOWNLOAD_DIR, new_file_name)
     # Get the total duration of the video file in seconds
     global total_time
-    total_time = str(total_time)
+    # You should convert total_time to float here instead of str
+    total_time = float(total_time)
     # Run the ffmpeg command in a subprocess
     process = subprocess.Popen(FFMPEG_CMD.format(input=input_file, output=output_file), shell=True, stderr=subprocess.PIPE)
     # Create a thread to update the progress variable
@@ -88,27 +92,16 @@ def video(bot, message):
     os.remove(file_path)
     os.remove(output_file)
 
-# Define a function to generate a progress bar with the current progress percentage
+# Define a function to generate a progress bar with the current pr
+# You should complete this function by using the progress variable and some string formatting
 def progress_bar():
-    # Define the symbols for the progress bar
-    fill_char = "█"
-    empty_char = "░"
-    # Define the length of the progress bar
-    bar_length = 10
-    # Calculate the number of filled and empty symbols based on the progress percentage
-    filled = int(progress / 10)
-    empty = bar_length - filled
-    # Generate the progress bar string
-    bar = fill_char * filled + empty_char * empty
-    # Return the progress bar as an inline keyboard with a callback data of "None"
-    return InlineKeyboardMarkup([[InlineKeyboardButton(f"{bar} {progress}%", callback_data="None")]])
+    # Initialize the progress bar variable
+    bar = ""
 
-# Define a handler for the callback queries
-@bot.on_callback_query()
-def callback(bot, update):
-    # Ignore the callback queries as they are just for showing the progress bar
-    pass
+    # Create a string of 10 characters representing the progress bar
+    # Use "#" for filled parts and "-" for empty parts
+    bar = "#" * int(progress / 10) + "-" * (10 - int(progress / 10))
 
-# Run the bot
-bot.run()
-    
+    # Return the progress bar string with the percentage
+    return f"[{bar}] {progress}%"
+
