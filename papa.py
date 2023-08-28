dccfb063f3870bfb9af5b2436bac2708e87e3fc3
itzmeproman <atulpandey3030@gmail.com>
2023-08-28 09:19:22 +0530

    Update papa.py

--- papa.py
+++ papa.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import threading
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Your bot token
 BOT_TOKEN = "YOUR_BOT_TOKEN"
